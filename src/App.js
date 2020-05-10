79d65810a47cb27b0dc8a0672edbed30b5381f32
MrFarhan <muhammadfarhan012345@gmail.com>
2020-05-10 18:54:09 +0500

    React Chat Widget

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ChatBot from 'react-simple-chatbot';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatBot
+        steps={[
+          {
+            id: '1',
+            message: 'Please type a number',
+            trigger: '2',
+          },
+          {
+            id: '2',
+            user: true,
+            validator: (value) => {
+              if (isNaN(value)) {
+                return 'value should be a number';
+              }
+              return true;
+            },
+            trigger: '1',
+          },
+        ]}
+      />
     </div>
   );
 }
